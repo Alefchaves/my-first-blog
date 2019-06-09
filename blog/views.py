2b8851d90b410409a91e2c22b60efb68166f96bb
alef <alef.chaves01@gmail.com>
2019-06-09 19:12:50 -0300

    subindo

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib import messages
 
 
 
@@ -15,6 +16,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    dele['delete'] = post
 
 
 def post_new(request):
@@ -33,6 +35,7 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -41,7 +44,16 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+
     else:
         form = PostForm(instance=post)
+
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_delete.html', {'object': post})
